e31f2c28d177e88a51a0c5aa0c192b60bfd8016a
SailorMarie <starnaud.mariepier@gmail.com>
2026-05-13 21:33:05 -0400

    fix player mouvement

--- Assets/Scripts/Controller/PlayerInputControler.cs
+++ Assets/Scripts/Controller/PlayerInputControler.cs
@@ -9,7 +9,7 @@ public class PlayerInputControler : MonoBehaviour
     [SerializeField]private float m_speed;
     [SerializeField]private int m_jumpForce;
     [SerializeField]private int m_maxDistance = 1000; //max distance du rayCast
-    
+    private const int DIRECTION = 10000;
    
     private Rigidbody m_body;
     private Vector2 m_movementInput;
@@ -70,12 +70,12 @@ public class PlayerInputControler : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 cameraFormardNoX = new Vector3(Camera.allCameras[0].transform.forward.x,0, Camera.allCameras[0].transform.forward.z);
-        transform.LookAt(cameraFormardNoX * 1000);
+        transform.LookAt(cameraFormardNoX * DIRECTION);
         
         Vector3 calculatedVelocity = m_body.linearVelocity;
         calculatedVelocity.x = m_movementInput.x * m_speed;
         calculatedVelocity.z = m_movementInput.y * m_speed;
-        m_body.linearVelocity = (transform.forward * calculatedVelocity.z + transform.right * calculatedVelocity.x + calculatedVelocity.y * Vector3.up);
+        m_body.linearVelocity = (transform.forward * calculatedVelocity.z + transform.right * calculatedVelocity.x + m_body.linearVelocity.y * Vector3.up);
 
          Vector3 move = Camera.allCameras[0].transform.forward * Input.GetAxis("Vertical") + Camera.allCameras[0].transform.right * Input.GetAxis("Horizontal");
         
@@ -92,6 +92,7 @@ public class PlayerInputControler : MonoBehaviour
         RaycastHit hitInfo;
         
         Debug.DrawRay(rayFromCamera.origin, rayFromCamera.direction * m_maxDistance, Color.red);
+        Debug.DrawRay(m_body.position, m_body.transform.forward * m_maxDistance, Color.green);
         if (Physics.Raycast(rayFromCamera, out hitInfo, m_maxDistance))
         {
             if (m_interact.WasPressedThisFrame())
