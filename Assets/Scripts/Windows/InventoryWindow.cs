711a9f0f9e7e8d68d965b5e80605960381add7b2
sailormarie <starnaud.mariepier@gmail.com>
2026-06-23 13:53:48 -0400

    inventory

--- Assets/Scripts/Windows/InventoryWindow.cs
+++ Assets/Scripts/Windows/InventoryWindow.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,11 @@ public class InventoryWindow : Window
 {
     private PlayerInventoryController m_playerInventory;
     [SerializeField] private List<Image> m_inventorySlot;
+    [SerializeField] private Image m_selectedItemIcon;
+    [SerializeField] private TextMeshProUGUI m_selectedItemName;
+    [SerializeField] private TextMeshProUGUI m_selectedItemDescription;
+
+    private Items m_currentSelectedItem = null;
 
     public void Initialize(PlayerInventoryController playerInventory)
     {
@@ -15,6 +21,25 @@ public class InventoryWindow : Window
         {
             m_inventorySlot[index].sprite = item.Sprite;
             index++;
+        }
+    }
+
+    public void ItemClick(int index)
+    {
+        Items item = m_playerInventory.GetAllItemsInInventory()[index];
+        if(item != null || item != m_currentSelectedItem)
+        {
+            m_selectedItemIcon.sprite = item.Sprite;
+            m_selectedItemName.text = item.Name;
+            m_selectedItemDescription.text = item.Description;
+            m_currentSelectedItem = item;
+        }
+        else
+        {
+            m_selectedItemIcon.sprite = null;
+            m_selectedItemName.text = "";
+            m_selectedItemDescription.text = "";
+            m_currentSelectedItem = null;
         }
     }
 }
