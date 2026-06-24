bcb959770a78a275c9290977fe72c6c41b23fc3c
sailormarie <starnaud.mariepier@gmail.com>
2026-06-24 18:08:01 -0400

    pop up

--- Assets/Scripts/Controller/GameSceneLevelController.cs
+++ Assets/Scripts/Controller/GameSceneLevelController.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameSceneLevelController : LevelController
 {
+    [SerializeField] Window m_tutoWindow;
+
+    
+    private Window m_currentWindow;
+
+    public override void ClosePopUp(InputAction.CallbackContext context)
+    {
+        if(context.performed && m_currentWindow != null)
+        {
+            m_currentWindow.Close();
+            m_currentWindow = null;
+
+        }
+    }
+
     public override void Init()
     {
         if (LevelManager.Instance.IsFirstTimeLevelLoaded(SceneManager.GetActiveScene().name))
         {
-            Debug.Log("[LEVEL]FirstTime");
+            m_currentWindow= UIManager.Instance.OpenWindow(m_tutoWindow);
         }
 
     }
 
     public override void SetDependencies(GameController gameController)
     {
-        
+     
     }
 }
--- Assets/Scripts/Controller/LevelController.cs
+++ Assets/Scripts/Controller/LevelController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public abstract class LevelController : MonoBehaviour
 {
     public abstract void SetDependencies(GameController gameController);
 
     public abstract void Init();
+
+    public abstract void ClosePopUp(InputAction.CallbackContext context);
     
 }
